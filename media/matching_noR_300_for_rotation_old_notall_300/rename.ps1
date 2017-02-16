ab04db72146e1390613ad5b50e3a7ac61ba85908
Vahan Yoghourdjian <vahan.yoghourdjian@gmail.com>
2017-02-16 16:47:24 +1100

    img sizes fixed good to go

---
+++ media/matching_noR_300_for_rotation_old_notall_300/rename.ps1
@@ -0,0 +1,21 @@
+﻿cd 'C:\Users\Vahan\Desktop\survey2\media\\matching_noR_300_for_rotation\1'
+Get-ChildItem -Path C:\Users\Vahan\Desktop\survey2\media\\matching_noR_300_for_rotation\1 -File -Recurse | ForEach-Object {
+ $myfilename = $_.Name
+ $mysp = $myfilename.IndexOf(".")
+ $mynewfilename = $myfilename.Substring(0,$mysp) + "1"+$myfilename.Substring($mysp)
+ ren $myfilename $mynewfilename
+}
+cd 'C:\Users\Vahan\Desktop\survey2\media\\matching_noR_300_for_rotation\2'
+Get-ChildItem -Path C:\Users\Vahan\Desktop\survey2\media\\matching_noR_300_for_rotation\2 -File -Recurse | ForEach-Object {
+ $myfilename = $_.Name
+ $mysp = $myfilename.IndexOf(".")
+ $mynewfilename = $myfilename.Substring(0,$mysp) + "2"+$myfilename.Substring($mysp)
+ ren $myfilename $mynewfilename
+}
+cd 'C:\Users\Vahan\Desktop\survey2\media\\matching_noR_300_for_rotation\3'
+Get-ChildItem -Path C:\Users\Vahan\Desktop\survey2\media\\matching_noR_300_for_rotation\3 -File -Recurse | ForEach-Object {
+ $myfilename = $_.Name
+ $mysp = $myfilename.IndexOf(".")
+ $mynewfilename = $myfilename.Substring(0,$mysp) + "3"+$myfilename.Substring($mysp)
+ ren $myfilename $mynewfilename
+}
